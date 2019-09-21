a8cfda28c02c1e805093c81187b62283170fdb3b
anloffredo <loffredo.ap@gmail.com>
2019-09-21 13:40:33 -0700

    moving some abstractions and simplifying method parameters

---
+++ GeneratedTemplateInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Web.Rendering
+{
+    /// <summary>
+    /// Represents the result of creating a view to be used with rendering an object model to a string
+    /// </summary>
+    public class GeneratedTemplateInfo
+    {
+        internal GeneratedTemplateInfo(string absolutePath, string relativePath, object model)
+        {
+            AbsolutePath = absolutePath;
+            RelativePath = relativePath;
+            Model = model;
+        }
+        /// <summary>
+        /// The absolute path to the generated template
+        /// </summary>
+        public string AbsolutePath { get; set; }
+
+        /// <summary>
+        /// The relative path to the generated template
+        /// </summary>
+        public string RelativePath { get; set; }
+
+        /// <summary>
+        /// The constructed object model to pass into the template
+        /// </summary>
+        public object Model { get; set; }
+    }
+}
